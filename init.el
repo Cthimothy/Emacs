864975decbc6344d2c75d96d3f2ef3f97360e230
Cthimothy <t.welch2@exeter.ac.uk>
2024-08-23 13:19:06 +0100

    Minor updates

--- init.el
+++ init.el
@@ -109,6 +109,7 @@
 
 ;; Set custom key bindings
 (global-set-key (kbd "C-x C-x") 'avy-goto-char-timer)
+(global-set-key (kbd "C-c o a") 'org-agenda)
 (global-set-key (kbd "C-c f") 'tw/dired-filter-files)
 (global-set-key (kbd "C-c b") 'ivy-switch-buffer-other-window)
 (global-set-key (kbd "C-c d s") 'dired-mark-files-regexp)
